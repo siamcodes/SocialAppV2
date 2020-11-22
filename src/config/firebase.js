3a6b220dddac2e6c8058481f8a3be8659dd72b12
siamcodes <siamcodes@gmail.com>
2020-11-22 14:31:35 +0700

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBkD0Ynlkymc2PJgqd68YcBSTvTgVQNZJ4",
+    authDomain: "social-app-dc870.firebaseapp.com",
+    databaseURL: "https://social-app-dc870.firebaseio.com",
+    projectId: "social-app-dc870",
+    storageBucket: "social-app-dc870.appspot.com",
+    messagingSenderId: "693200799514",
+    appId: "1:693200799514:web:faa36dbf8c5ed1f5064ea3"
+};
